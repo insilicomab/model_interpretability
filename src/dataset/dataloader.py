97c29fe39dc21a810e5cb8547c7aacecc0d5dd2c
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2024-03-06 10:33:50 +0900

    追加

---
+++ src/dataset/dataloader.py
@@ -0,0 +1,25 @@
+import numpy as np
+import torchvision
+from PIL import Image
+from torchvision import transforms
+
+
+# class to load input images and processed original images
+class ImageLoader:
+
+    def __init__(self, img_dir: str, transform=None):
+        self.img_dir = img_dir
+        self.transform = transform
+
+    def process_images(self):
+
+        # read an image file
+        img = Image.open(self.img_dir).convert("RGB")
+
+        # process input image
+        input_img = self.transform("input", img).unsqueeze(0)
+
+        # process original image
+        original_img = np.array(self.transform("original", img))
+
+        return input_img, original_img
